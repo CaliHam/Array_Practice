b7c50d5936f0944945f2b44139dfeb888871e134
Calli Herrmann <calliherrmann@gmail.com>
2023-03-01 17:49:31 -0500

    Add methods and descriptions to the arrays

--- arrays.js
+++ arrays.js
@@ -2,3 +2,12 @@ var friends = ["Bobby", "Bailey", "Bethany", "Doriana"];
 var ages = [29, 21, 26, 23];
 var female = [false, true, true, true];
 
+// The .pop method will remove "Doriana" from the friends array
+friends.pop();
+
+// The .push method will add 30 to the end of the array
+ages.push(30);
+
+// The .shift method will remove the element in the first index position which would be "false"
+female.shift();
+
